bfba2fea9e840327c934bb611c279dd506155dcf
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-12-11 20:38:21 -0700

    adding useradmin redirects

---
+++ views/useradmin.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+if (basename($_SERVER['SCRIPT_NAME']) == basename(__FILE__))
+{
+    header('Location: /anthony1/phprouter');
+}
+
+if(!$_SESSION['loggedIn']){
+    header('Location: /superuser/phprouter/login');
+} else if($_SESSION['level'] !== 'admin'){
+    echo 'You are not an admin';
+} 
+?>
